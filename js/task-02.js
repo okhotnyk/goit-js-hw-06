8bc8ff5741fe6599b659980fe0bb42bc389be4b3
Kateryna Okhotnyk <k.okhotnyk@gmail.com>
2022-07-27 01:35:55 +0300

    create and adds elements

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const allListItems = [];
+const ulIngredientsEl = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = ingredient;
+  listItem.classList.add("item");
+  allListItems.push(listItem);
+});
+
+ulIngredientsEl.append(...allListItems);
