fbcece0279d44c253bd9ecf48824c76d0538199a
Kateryna Okhotnyk <k.okhotnyk@gmail.com>
2022-07-26 14:02:10 +0300

    categories count added

---
+++ js/task-01.js
@@ -0,0 +1,21 @@
+const catNumberEl = document.querySelectorAll(".item").length;
+console.log("Number of categories: ", catNumberEl);
+
+// const catNumberRef = document.querySelector("#categories");
+// console.log("Number of categories: ", catNumberRef.childElementCount);
+
+// Category: Animals;
+// Elements: 4;
+
+// Category: Products;
+// Elements: 3;
+
+// Category: Technologies;
+// Elements: 5;
+
+const nameOfCategory = document.querySelector("#categories");
+console.log(nameOfCategory.childElementCount);
+
+// nameOfCategory.forEach((category) => {
+//   console.dir("Category: ", category);
+// });
