10fd5f6accce4fa6331400370325e0dadec19a4e
Kateryna Okhotnyk <k.okhotnyk@gmail.com>
2022-07-30 16:50:16 +0300

    body style bgc added

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const backgroundColor = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+
+changeColorBtn.addEventListener("click", onButtonChageColorClick);
+
+function onButtonChageColorClick() {
+  backgroundColor.textContent = getRandomHexColor();
+  bodyEl.style.backgroundColor = backgroundColor.textContent;
+}
