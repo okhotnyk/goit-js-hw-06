00729be95d5c79b6b6fecea4ea5c15b188b6ad2d
Kateryna Okhotnyk <k.okhotnyk@gmail.com>
2022-07-30 16:08:20 +0300

    object of elements added

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const submitForm = document.querySelector(".login-form");
 
+submitForm.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  } else {
+    let objectDataForm = { email: email.value, password: password.value };
+    console.log(objectDataForm);
+    event.currentTarget.reset();
+  }
+}
