e769d56a915b964b6a83a1203225b76db93d0afa
Kateryna Okhotnyk <k.okhotnyk@gmail.com>
2022-07-31 00:31:37 +0300

    buttons added

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,33 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputEl = document.querySelector("input");
+const buttonCreateEl = document.querySelector("button[data-create]");
+const buttonDestroyEl = document.querySelector("button[data-destroy]");
+const wrapper = document.querySelector("#boxes");
+let boxAmount;
+
+inputEl.addEventListener("input", onInputChange);
+buttonCreateEl.addEventListener("click", createBoxes);
+buttonDestroyEl.addEventListener("click", destroyBoxes);
+
+function onInputChange(event) {
+  boxAmount = event.currentTarget.value;
+}
+
+function createBoxes(event) {
+  let allCreatedBoxes = [];
+  for (let i = 0; i < boxAmount; i += 1) {
+    const box = document.createElement("div");
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${20 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    allCreatedBoxes.push(box);
+    wrapper.append(...allCreatedBoxes);
+  }
+}
+
+function destroyBoxes(event) {
+  wrapper.innerHTML = "";
+}
